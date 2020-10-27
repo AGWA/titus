AGWA/titus

commit ac1dd613e9900f5a2de8f374206d31cbf46acb66

Don't use AI_V4MAPPED, which isn't supported by FreeBSD

util.cpp
@@ -198,9 +198,9 @@ void resolve_address (struct sockaddr_in6* address, const std::string& host, con
 {
 	struct addrinfo		hints;
 	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET6;
+	hints.ai_family = host.empty() ? AF_INET6 : AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_V4MAPPED;
+	hints.ai_flags = AI_PASSIVE;
 	hints.ai_protocol = 0;
 
 	struct addrinfo*	addrs;
@@ -212,9 +212,17 @@ void resolve_address (struct sockaddr_in6* address, const std::string& host, con
 		freeaddrinfo(addrs);
 		throw Configuration_error("[" + host + "]:" + port + " resolves to more than one address");
 	}
-	std::memcpy(address, addrs->ai_addr, sizeof(*address));
-	if (host.empty()) {
-		std::memcpy(&address->sin6_addr, &in6addr_any, sizeof(in6_addr));
+	if (addrs->ai_family == AF_INET) {
+		std::memset(address, '\0', sizeof(*address));
+		address->sin6_addr.s6_addr[10] = 0xFF;
+		address->sin6_addr.s6_addr[11] = 0xFF;
+		std::memcpy(&address->sin6_addr.s6_addr[12], &reinterpret_cast<const sockaddr_in*>(addrs->ai_addr)->sin_addr, 4);
+		address->sin6_port = reinterpret_cast<const sockaddr_in*>(addrs->ai_addr)->sin_port;
+	} else if (addrs->ai_family == AF_INET6) {
+		std::memcpy(address, addrs->ai_addr, sizeof(*address));
+	} else {
+		freeaddrinfo(addrs);
+		throw Configuration_error("[" + host + "]:" + port + " resolves to an unknown address family");
 	}
 	if (port.empty()) {
 		address->sin6_port = htons(0);

commit 2b90c30400c0bb204bb6d045fea469a69af3bc07

Set sin6_family when resolving IPv4 address
Fixes regression introduced by ac1dd613e9900f5a2de8f374206d31cbf46acb66

util.cpp
@@ -230,6 +230,7 @@ void resolve_address (struct sockaddr_in6* address, const std::string& host, con
 	}
 	if (addrs->ai_family == AF_INET) {
 		std::memset(address, '\0', sizeof(*address));
+		address->sin6_family = AF_INET6;
 		address->sin6_addr.s6_addr[10] = 0xFF;
 		address->sin6_addr.s6_addr[11] = 0xFF;
 		std::memcpy(&address->sin6_addr.s6_addr[12], &reinterpret_cast<const sockaddr_in*>(addrs->ai_addr)->sin_addr, 4);